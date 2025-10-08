d9c4d20228992fd0d18881c31016f2dcfe351ab1
ThaksinhCW3 <thaksin.mnv785@gmail.com>
2025-10-08 12:48:36 +0700

    web route hotfixes

--- routes/web.php
+++ routes/web.php
@@ -45,11 +45,6 @@ Route::middleware('auth')->group(function () {
     Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
 
-    // Profile routes
-    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::put('/profile/password', [ProfileController::class, 'updatePassword'])->name('profile.password.update');
-
     // Verification routes - protected by ban check
     Route::middleware('check.banned')->group(function () {
         Route::get('/profile/verification', [VerificationController::class, 'index'])->name('verification.index');
